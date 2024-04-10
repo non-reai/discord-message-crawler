79b69beb4f05ddf383a073696c94df22a56e2cbc
Non_Reai <63729314+non-reai@users.noreply.github.com>
2024-04-10 09:42:52 -0400

    formatted index.js

--- index.js
+++ index.js
@@ -9,53 +9,53 @@ let lastCrawledMessage = ""
 let messagesCrawled = 0
 
 async function crawlChannel(messageId) {
-	const response = await fetch(`https://discord.com/api/v9/channels/${CHANNEL_ID}/messages?before=${messageId}&limit=100`, {
-		"headers": {
-			"authorization": DISCORD_TOKEN,
-		},
-		"method": "GET",
-	});
+    const response = await fetch(`https://discord.com/api/v9/channels/${CHANNEL_ID}/messages?before=${messageId}&limit=100`, {
+        "headers": {
+            "authorization": DISCORD_TOKEN,
+        },
+        "method": "GET",
+    });
 
     if (response.status == "429") {
-        console.log("ratelimited "+Number(response.headers.get("X-RateLimit-Reset-After")) * 1000)
-        setTimeout(()=>{
+        console.log("ratelimited " + Number(response.headers.get("X-RateLimit-Reset-After")) * 1000)
+        setTimeout(() => {
             crawlChannel(messageId)
         }, Number(response.headers.get("X-RateLimit-Reset-After")) * 1000 + 1000)
     }
-	
-	const parsedMessages = await response.json()
-	
+
+    const parsedMessages = await response.json()
+
     if (messagesCrawled == 0) {
         fs.writeFileSync("messages.json", "[")
-    } 
+    }
 
     parsedMessages.forEach((element, index) => {
         messagesCrawled++
-        fs.appendFileSync("messages.json","\n"+JSON.stringify(element,null,2))
+        fs.appendFileSync("messages.json", "\n" + JSON.stringify(element, null, 2))
         if (!(parsedMessages.length < 100) || !(index == parsedMessages.length - 1)) {
-            fs.appendFileSync("messages.json",",")
+            fs.appendFileSync("messages.json", ",")
         }
         lastCrawledMessage = element.id
     });
 
     if (parsedMessages.length < 100) {
-        fs.appendFileSync("messages.json","\n]")
+        fs.appendFileSync("messages.json", "\n]")
         return
     }
 
     console.log(parsedMessages)
 
-    setTimeout(()=>{
+    setTimeout(() => {
         crawlChannel(parsedMessages[parsedMessages.length - 1].id)
-    },250)
+    }, 250)
 }
 
 crawlChannel(STARTING_MESSAGE_ID)
 
 process.on('SIGINT', function() {
-    console.log("\nGracefully shutting down from SIGINT (Ctrl-C)" );
-    console.log("Messages Crawled: "+messagesCrawled)
-    console.log("Last Crawled Message Id: "+lastCrawledMessage)
+    console.log("\nGracefully shutting down from SIGINT (Ctrl-C)");
+    console.log("Messages Crawled: " + messagesCrawled)
+    console.log("Last Crawled Message Id: " + lastCrawledMessage)
 
     process.exit();
 })
